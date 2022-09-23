01ba9457d8250bbc5f8f01eaf640a106de30c3da
Pedro Tanamachi <pedroeduardotanamachigarza@gmail.com>
2022-09-23 12:49:11 -0600

    Movimiento basico y control de camara agregados

--- minecraft-freecodecamp-start/src/App.js
+++ minecraft-freecodecamp-start/src/App.js
@@ -1,11 +1,23 @@
-import { Canvas } from '@react-three/fiber';
+import { Physics } from "@react-three/cannon";
+import { Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Ground } from "./components/Ground";
+import { Player } from "./components/Player";
+import { FPV } from "./components/FPV";
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
       <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <FPV />
+        <Physics>
+          <Player />
+          <Ground />
+        </Physics>
       </Canvas>
+      <div className="absolute center cursor">+</div>
     </>
   );
 }
